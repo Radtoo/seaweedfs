17b390ef0ac530b700f986ce487c12168bd813c8
Radtoo <56451+Radtoo@users.noreply.github.com>
2022-01-07 19:17:57 +0100

    Trying to use paths for weed fix.

--- weed/command/fix.go
+++ weed/command/fix.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"path"
 	"strconv"
+	//"fmt"
+	"regexp"
 
 	"github.com/chrislusf/seaweedfs/weed/glog"
 	"github.com/chrislusf/seaweedfs/weed/storage"
@@ -19,18 +21,12 @@ func init() {
 }
 
 var cmdFix = &Command{
-	UsageLine: "fix -dir=/tmp -volumeId=234",
-	Short:     "run weed tool fix on index file if corrupted",
-	Long: `Fix runs the SeaweedFS fix command to re-create the index .idx file.
+	UsageLine: "fix /path/to/volume.dat [/path/to/another/volume.dat]",
+	Short:     "run to recreate index .idx files of volume .dat files",
+	Long: `Fix runs the SeaweedFS fix command on volume .dat files to re-create their index .idx files.
 
   `,
 }
-
-var (
-	fixVolumePath       = cmdFix.Flag.String("dir", ".", "data directory to store files")
-	fixVolumeCollection = cmdFix.Flag.String("collection", "", "the volume collection name")
-	fixVolumeId         = cmdFix.Flag.Int("volumeId", -1, "a volume id. The volume should already exist in the dir. The volume index file should not exist.")
-)
 
 type VolumeFileScanner4Fix struct {
 	version needle.Version
@@ -42,6 +38,7 @@ func (scanner *VolumeFileScanner4Fix) VisitSuperBlock(superBlock super_block.Sup
 	return nil
 
 }
+
 func (scanner *VolumeFileScanner4Fix) ReadNeedleBody() bool {
 	return false
 }
@@ -59,34 +56,65 @@ func (scanner *VolumeFileScanner4Fix) VisitNeedle(n *needle.Needle, offset int64
 }
 
 func runFix(cmd *Command, args []string) bool {
+	//args := []string{"/path/to/data_1.dat", "/1.dat", "/path/to/2.dat", "./collection_24.dat", "./path/to/42.dat"}
+	re := regexp.MustCompile(`^(?P<path>\.?/(?:.+/)*)(?P<filename>(?:(?P<collection>.+?)_)?(?P<volumeId>[\d]+))\.(?P<extension>.+)$`)
+	for _, arg := range args {
+		
+		if _, err := os.Stat(arg); os.IsNotExist(err) {
+			glog.V(2).Infof("skipping nonextant file: %s", arg)
+			continue;
+		}
 
-	if *fixVolumeId == -1 {
-		return false
+		m := re.FindStringSubmatch(arg)
+
+		//fmt.Printf("Argument: \"%s\"\n", arg)
+		//fmt.Printf("Subexpressions/Captured groups: %#v\n", re.SubexpNames())
+		//fmt.Printf("Resulting Match: %#v\n", m)
+
+		if m[4] == "" {
+			glog.V(2).Infof("volumeId did parse as empty instead of a sequence of digits, unexpected file naming and/or parsing issue")
+			continue;
+		}
+
+		fixVolumePath := m[1]
+		fixFilename := m[2]
+		fixVolumeCollection := m[3]
+		fixVolumeId,_ := strconv.Atoi(m[4])
+		fixExtension := m[5]
+
+		if fixExtension != "dat" {
+			glog.V(2).Infof("extension did not parse as dat: %s , skipping %s", fixExtension, arg)
+			continue;
+		}
+		if fixVolumePath == "" {
+			glog.V(2).Infof("volume path did parse empty, please use a path")
+			continue;
+		}
+		if fixFilename == "" {
+			glog.V(2).Infof("filename did parse as empty, unexpected file naming and/or parsing issue")
+			continue;
+		}
+
+
+		indexFileName := path.Join(util.ResolvePath(fixVolumePath), fixFilename+".idx")
+
+		nm := needle_map.NewMemDb()
+		defer nm.Close()
+
+		vid := needle.VolumeId(fixVolumeId)
+		scanner := &VolumeFileScanner4Fix{
+			nm: nm,
+		}
+
+		if err := storage.ScanVolumeFile(util.ResolvePath(fixVolumePath), fixVolumeCollection, vid, storage.NeedleMapInMemory, scanner); err != nil {
+			glog.Fatalf("scan .dat File: %v", err)
+			os.Remove(indexFileName)
+		}
+
+		if err := nm.SaveToIdx(indexFileName); err != nil {
+			glog.Fatalf("save to .idx File: %v", err)
+			os.Remove(indexFileName)
+		}
 	}
-
-	baseFileName := strconv.Itoa(*fixVolumeId)
-	if *fixVolumeCollection != "" {
-		baseFileName = *fixVolumeCollection + "_" + baseFileName
-	}
-	indexFileName := path.Join(util.ResolvePath(*fixVolumePath), baseFileName+".idx")
-
-	nm := needle_map.NewMemDb()
-	defer nm.Close()
-
-	vid := needle.VolumeId(*fixVolumeId)
-	scanner := &VolumeFileScanner4Fix{
-		nm: nm,
-	}
-
-	if err := storage.ScanVolumeFile(util.ResolvePath(*fixVolumePath), *fixVolumeCollection, vid, storage.NeedleMapInMemory, scanner); err != nil {
-		glog.Fatalf("scan .dat File: %v", err)
-		os.Remove(indexFileName)
-	}
-
-	if err := nm.SaveToIdx(indexFileName); err != nil {
-		glog.Fatalf("save to .idx File: %v", err)
-		os.Remove(indexFileName)
-	}
-
 	return true
 }
